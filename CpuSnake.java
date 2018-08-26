7d1275e8f8a63505e9c4db0ec849dc3f2d6b8eb0
cm <cmsingh1721997@gmail.com>
2018-08-26 18:24:03 +0530

    Add files via upload

---
+++ CpuSnake.java
@@ -0,0 +1,36 @@
+package cpuSnake;
+
+    import snakegamemultiplayer.SnakeGameConstants;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import commonUtils.Snake; 
+
+
+
+  public class CpuSnake extends Snake implements SnakeGameConstants{
+  
+	  public CpuSnake() {
+		 
+	} 
+
+	  public CpuSnake(int x ,int y) {
+		 super(x,y);
+	} 
+	  
+	  
+	public void drawsnakebody(Graphics g){
+			  
+			 g.setColor(Color.black);
+			// g.fillRoundRect(,);
+			 g.fillRect(getX()+1,getY()+2,SNAKEBODYWIDTH-3,SNAKEBODYHEIGHT-3);
+			 
+		} 
+	
+	
+	
+	
+	   
+}
